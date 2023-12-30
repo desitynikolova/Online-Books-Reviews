80258013d65f896628aaf22853ef10b8f76df00d
Desislava Nikolova <desi011@abv.bg>
2023-12-30 16:48:18 +0200

    add comments to BookRegistration

--- src/main/java/com/online/book/store/configuration/BookRegistration.java
+++ src/main/java/com/online/book/store/configuration/BookRegistration.java
@@ -3,39 +3,39 @@ package com.online.book.store.configuration;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+
+// Този клас представлява регистрация на книга
 @Entity
 public class BookRegistration {
 
     @Id
-    String Book_title;
+    String Book_title; // декларира поле за съхранение на заглавието на книгата
 
-    String Author;
+    String Author; // декларира поле за съхранение на автора на книгата
 
-    String rate;
+    String rate; // декларира поле за съхранение на рейтинга на книгата
 
     public String getBook_title() {
         return Book_title;
-    }
+    } // дефинира метод за връщане на стойността на полето Book_title
 
     public void setBook_title(String book_title) {
         Book_title = book_title;
-    }
+    } // този метод приема като аргумент новата стойност за Book_title и я присвоява на полето
 
     public String getAuthor() {
         return Author;
-    }
+    } // този метод връща текущата стойност на Author; използва се за получаване на стойности на Author
 
     public void setAuthor(String author) {
         Author = author;
-    }
+    } // този метод приема като аргумент новата стойност за Author и я присвоява на полето
 
     public String getRate() {
         return rate;
-    }
+    } // този метод връща текущата стойност на Rate; използва се за получаване на стойности на Rate
 
     public void setRate(String rate) {
         this.rate = rate;
-    }
-
-
+    } // този метод приема като аргумент новата стойност за Rate и я присвоява на полето
 }
