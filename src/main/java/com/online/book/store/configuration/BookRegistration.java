9c04972e5df0f3624be65b7a5ba1b2013f0ed0aa
Desislava Nikolova <desi011@abv.bg>
2023-12-31 15:36:22 +0200

    add comments to BookRegistration and UserRegistration

--- src/main/java/com/online/book/store/configuration/BookRegistration.java
+++ src/main/java/com/online/book/store/configuration/BookRegistration.java
@@ -3,11 +3,9 @@ package com.online.book.store.configuration;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-
 // Този клас представлява регистрация на книга
 @Entity
 public class BookRegistration {
-
     @Id
     String Book_title; // декларира поле за съхранение на заглавието на книгата
 
@@ -15,27 +13,37 @@ public class BookRegistration {
 
     String rate; // декларира поле за съхранение на рейтинга на книгата
 
+    String Price; // декларира поле за съхранение на цената на книгата
+
+    public String getPrice() {
+        return Price;
+    } // дефинира метод за връщане на стойността на полето price
+
+    public void setPrice(String price) {
+        Price = price;
+    }  // този метод приема като аргумент новата стойност за price и я присвоява на полето
+
     public String getBook_title() {
         return Book_title;
     } // дефинира метод за връщане на стойността на полето Book_title
 
     public void setBook_title(String book_title) {
         Book_title = book_title;
-    } // този метод приема като аргумент новата стойност за Book_title и я присвоява на полето
+    }  // този метод приема като аргумент новата стойност за Book_title и я присвоява на полето
 
     public String getAuthor() {
         return Author;
-    } // този метод връща текущата стойност на Author; използва се за получаване на стойности на Author
+    } // дефинира метод за връщане на стойността на полето Author
 
     public void setAuthor(String author) {
         Author = author;
-    } // този метод приема като аргумент новата стойност за Author и я присвоява на полето
+    }  // този метод приема като аргумент новата стойност за Author и я присвоява на полето
 
     public String getRate() {
         return rate;
-    } // този метод връща текущата стойност на Rate; използва се за получаване на стойности на Rate
+    } // дефинира метод за връщане на стойността на полето Rate
 
     public void setRate(String rate) {
         this.rate = rate;
-    } // този метод приема като аргумент новата стойност за Rate и я присвоява на полето
+    }  // този метод приема като аргумент новата стойност за Rate и я присвоява на полето
 }
--- src/main/java/com/online/book/store/configuration/UserRegistration.java
+++ src/main/java/com/online/book/store/configuration/UserRegistration.java
@@ -1,6 +1,5 @@
 package com.online.book.store.configuration;
 
-
 import java.sql.Date;
 
 import jakarta.persistence.Entity;
@@ -11,11 +10,10 @@ public class UserRegistration {
 
 	@Id
 	private String email; // полето email се анотира като основен ключ
-
 	private String fullname; // поле за пълното име на потребителя
 	private String password; // поле за паролата на потребителя
-	private Date date; // поле за рожденна дата на потребителя
-	private String gender; // поле за пол (мъж/жена)
+	private Date date; // поле за рожденната дата на потребителя
+	private String gender; // поле за пола (мъж/жена) на потребителя
 	
 	public String getEmail() {
 		return email;
@@ -47,11 +45,12 @@ public class UserRegistration {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
+	
 	// Методът toString връща символен низ, който съдържа стойностите на всички полета в класа UserRegistration
 	@Override
 	public String toString() {
 		return "UserRegistration [email=" + email + ", fullname=" + fullname + ", password=" + password + ", date="
 				+ date + ", gender=" + gender + "]";
 	}
+
 }
