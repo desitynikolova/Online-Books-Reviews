e1e0041369a15eb62672903d5931d19a162688e9
Desislava Nikolova <desi011@abv.bg>
2024-01-03 11:56:52 +0200

    update

--- src/main/java/com/online/book/review/repository/BookRepo.java
+++ src/main/java/com/online/book/review/repository/BookRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.online.book.review.model.BookRegistration;
 
+// Интерфейс, който наследява CrudRepository и служи за взаимодействие с базата данни за книги (H2 Database)
+
 // съхранение на информация за книги
 public interface BookRepo extends CrudRepository<BookRegistration, String> {
     List<BookRegistration> findAll();
--- src/main/java/com/online/book/review/repository/UserRepo.java
+++ src/main/java/com/online/book/review/repository/UserRepo.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.online.book.review.model.UserRegistration;
 
+// Интерфейс, който наследява CrudRepository и служи за взаимодействие с базата данни за потребители (H2 Database)
+
 // съхранение на регистрирани потребители
 public interface UserRepo extends CrudRepository<UserRegistration, String> {
     public UserRegistration findByEmailAndPassword(String email, String password);
