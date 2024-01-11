2573ad29fc89770f79a07d629e73b5ceb992fcf8
Desislava Nikolova <desi011@abv.bg>
2024-01-11 20:32:02 +0200

    update

--- src/main/java/com/online/book/review/repository/BookRepo.java
+++ src/main/java/com/online/book/review/repository/BookRepo.java
@@ -9,6 +9,7 @@ import com.online.book.review.model.BookRegistration;
 // Интерфейс, който наследява CrudRepository и служи за взаимодействие с базата данни за книги (H2 Database)
 
 // съхранение на информация за книги
+// Този интерфейс предоставя основните операции за управление на данни (CRUD - Create, Read, Update, Delete) върху обекти от тип BookRegistration
 public interface BookRepo extends CrudRepository<BookRegistration, String> {
     List<BookRegistration> findAll();
 }
